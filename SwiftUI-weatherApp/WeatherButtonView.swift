e375fb277c33ac06e877ebb26019f89743871bf2
Maheshi <maheshianuradha2000@gmail.com>
2024-10-10 15:20:23 +0530

    first commit

---
+++ SwiftUI-weatherApp/WeatherButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherButtonView.swift
+//  SwiftUI-weatherApp
+//
+//  Created by Maheshi Anuradha on 2024-10-10.
+//
+
+import SwiftUI
+
+struct WeatherButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherButtonView()
+}
