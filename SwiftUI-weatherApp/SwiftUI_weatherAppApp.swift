c6b79c376e8c019cdbb1b899d7945250b3783e88
Maheshi <maheshianuradha2000@gmail.com>
2024-10-10 09:51:43 +0530

    Initial Commit

---
+++ SwiftUI-weatherApp/SwiftUI_weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherAppApp.swift
+//  SwiftUI-weatherApp
+//
+//  Created by Maheshi Anuradha on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
